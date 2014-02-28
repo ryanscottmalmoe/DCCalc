e4b0ae209720aa31b3bfd52f7826f6037e9be4ba
ryanscottmalmoe <ryanmalmoe@gmail.com>
2014-02-27 16:12:47 -0800

    Initial Commit

---
+++ DCCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DCCalc
+//
+//  Created by Ryan Malmoe on 2/27/14.
+//  Copyright (c) 2014 Ryan Malmoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
